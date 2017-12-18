996947563e5ea0c8169215d67ab7dcd4cff07281
Jianing Yang <jianingy.yang@gmail.com>
2017-12-18 17:28:34 +0800

    Bump version: 0.1.11 → 0.1.12

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ import setuptools
 
 setuptools.setup(
     name='prestornado',
-    version='0.1.11',
+    version='0.1.12',
     description='Asynchronous PrestoDB DB-API for Tornado Web Server',
     author='Jianing Yang',
     author_email='jianingy.yang@gmail.com',
